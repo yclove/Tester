71cdd5c7014b9831cf2629db22fa716be50b7eac
mononokeo <mononok@naver.com>
2019-06-19 02:24:56 +0900

    TEST

--- app/src/main/java/com/ycengine/tester/MainActivity.kt
+++ app/src/main/java/com/ycengine/tester/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
 
+        
         initViewModel()
     }
 
